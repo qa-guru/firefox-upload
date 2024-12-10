f560eefc87dcc8730a80fb23777ce1e1e0b137d2
svasenkov <aanher@gmail.com>
2024-12-10 18:43:45 +0100

    updated selenoid vnc

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,7 +1,4 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Step;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,10 +6,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
-import static io.qameta.allure.Allure.step;
 
 public class FormTest {
 
@@ -22,6 +17,14 @@ public class FormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.browserVersion = "125.0";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", false
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @Test
@@ -30,6 +33,7 @@ public class FormTest {
 
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
         $("#uploadPicture").uploadFromClasspath("img/1.png");
     }
 
@@ -39,6 +43,7 @@ public class FormTest {
 
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
         $("#uploadPicture").uploadFromClasspath("img/1.png");
     }
 
